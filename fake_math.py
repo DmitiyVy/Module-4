12e961ab0686f4b208b65fc25e558dd391a1aaa7
DmitiyVy <dmitriy.jons@gmail.com>
2024-12-13 21:23:27 +0700

    module_4_1.py

---
+++ fake_math.py
@@ -0,0 +1,12 @@
+def divide(first, second):
+    result = 0
+    if second == 0:
+        result = "Ошибка"
+        return result
+    else:
+        result += first / second
+        return result
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(first, second):
+    result = 0
+    if second == 0:
+        return inf
+    else:
+        result += first / second
+        return result
